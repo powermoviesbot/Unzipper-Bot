3afe4786916f601bcb203bae8d25c36b5622da70
powermoviesbot <114382093+powermoviesbot@users.noreply.github.com>
2023-03-21 18:45:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ VERSION="v1.0"
 echo "
     ||| Unzipper Bot - $VERSION |||
 
-Copyright (c) 2022 Itz-fork
+Copyright (c) 2023 PowerMovies
 
---> Join @NexaBotsUpdates
+--> Join @PowerMoviesUpdates
 --> Follow Itz-fork on Github
 "
 python3 -m unzipper
